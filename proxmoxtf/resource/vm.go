7d4ec5241f7160ee8280b73956439d52d3bb2352
hrmny <github.leah@hrmny.sh>
2023-06-25 19:54:41 +0200

    add import support for a lot of resources

--- proxmoxtf/resource/vm.go
+++ proxmoxtf/resource/vm.go
@@ -1271,6 +1271,22 @@ func VM() *schema.Resource {
 				},
 			),
 		),
+		Importer: &schema.ResourceImporter{
+			StateContext: func(ctx context.Context, d *schema.ResourceData, i interface{}) ([]*schema.ResourceData, error) {
+				node, id, err := parseImportIdWithNodeName(d.Id())
+				if err != nil {
+					return nil, err
+				}
+
+				d.SetId(id)
+				err = d.Set(mkResourceVirtualEnvironmentVMNodeName, node)
+				if err != nil {
+					return nil, err
+				}
+
+				return []*schema.ResourceData{d}, nil
+			},
+		},
 	}
 }
 
