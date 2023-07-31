f277fdfea6cc8f96bd2230fa576f663f0a9ba28f
hrmny <github.leah@hrmny.sh>
2023-08-01 01:21:03 +0200

    remove nested ACLs

--- proxmoxtf/resource/group.go
+++ proxmoxtf/resource/group.go
@@ -13,7 +13,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
-	"github.com/bpg/terraform-provider-proxmox/internal/types"
 	"github.com/bpg/terraform-provider-proxmox/proxmox/access"
 	"github.com/bpg/terraform-provider-proxmox/proxmoxtf"
 )
@@ -21,47 +20,15 @@ import (
 const (
 	dvResourceVirtualEnvironmentGroupComment = ""
 
-	mkResourceVirtualEnvironmentGroupACL          = "acl"
-	mkResourceVirtualEnvironmentGroupACLPath      = "path"
-	mkResourceVirtualEnvironmentGroupACLPropagate = "propagate"
-	mkResourceVirtualEnvironmentGroupACLRoleID    = "role_id"
-	mkResourceVirtualEnvironmentGroupComment      = "comment"
-	mkResourceVirtualEnvironmentGroupID           = "group_id"
-	mkResourceVirtualEnvironmentGroupMembers      = "members"
+	mkResourceVirtualEnvironmentGroupComment = "comment"
+	mkResourceVirtualEnvironmentGroupID      = "group_id"
+	mkResourceVirtualEnvironmentGroupMembers = "members"
 )
 
 // Group returns a resource that manages a group in the Proxmox VE access control list.
 func Group() *schema.Resource {
 	return &schema.Resource{
 		Schema: map[string]*schema.Schema{
-			mkResourceVirtualEnvironmentGroupACL: {
-				Type:        schema.TypeSet,
-				Description: "The access control list",
-				Optional:    true,
-				DefaultFunc: func() (interface{}, error) {
-					return []interface{}{}, nil
-				},
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						mkResourceVirtualEnvironmentGroupACLPath: {
-							Type:        schema.TypeString,
-							Required:    true,
-							Description: "The path",
-						},
-						mkResourceVirtualEnvironmentGroupACLPropagate: {
-							Type:        schema.TypeBool,
-							Optional:    true,
-							Description: "Whether to propagate to child paths",
-							Default:     false,
-						},
-						mkResourceVirtualEnvironmentGroupACLRoleID: {
-							Type:        schema.TypeString,
-							Required:    true,
-							Description: "The role id",
-						},
-					},
-				},
-			},
 			mkResourceVirtualEnvironmentGroupComment: {
 				Type:        schema.TypeString,
 				Description: "The group comment",
@@ -93,6 +60,7 @@ func Group() *schema.Resource {
 
 func groupCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	config := m.(proxmoxtf.ProviderConfiguration)
+
 	api, err := config.GetClient()
 	if err != nil {
 		return diag.FromErr(err)
@@ -113,29 +81,6 @@ func groupCreate(ctx context.Context, d *schema.ResourceData, m interface{}) dia
 
 	d.SetId(groupID)
 
-	aclParsed := d.Get(mkResourceVirtualEnvironmentGroupACL).(*schema.Set).List()
-
-	for _, v := range aclParsed {
-		aclDelete := types.CustomBool(false)
-		aclEntry := v.(map[string]interface{})
-		aclPropagate := types.CustomBool(
-			aclEntry[mkResourceVirtualEnvironmentGroupACLPropagate].(bool),
-		)
-
-		aclBody := &access.ACLUpdateRequestBody{
-			Delete:    &aclDelete,
-			Groups:    []string{groupID},
-			Path:      aclEntry[mkResourceVirtualEnvironmentGroupACLPath].(string),
-			Propagate: &aclPropagate,
-			Roles:     []string{aclEntry[mkResourceVirtualEnvironmentGroupACLRoleID].(string)},
-		}
-
-		err := api.Access().UpdateACL(ctx, aclBody)
-		if err != nil {
-			return diag.FromErr(err)
-		}
-	}
-
 	return groupRead(ctx, d, m)
 }
 
@@ -143,12 +88,14 @@ func groupRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.
 	var diags diag.Diagnostics
 
 	config := m.(proxmoxtf.ProviderConfiguration)
+
 	api, err := config.GetClient()
 	if err != nil {
 		return diag.FromErr(err)
 	}
 
 	groupID := d.Id()
+
 	group, err := api.Access().GetGroup(ctx, groupID)
 	if err != nil {
 		if strings.Contains(err.Error(), "HTTP 404") {
@@ -156,42 +103,16 @@ func groupRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.
 
 			return nil
 		}
+
 		return diag.FromErr(err)
 	}
-
-	acl, err := api.Access().GetACL(ctx)
-	if err != nil {
-		return diag.FromErr(err)
-	}
-
-	var aclParsed []interface{}
-
-	for _, v := range acl {
-		if v.Type == "group" && v.UserOrGroupID == groupID {
-			aclEntry := map[string]interface{}{}
-
-			aclEntry[mkResourceVirtualEnvironmentGroupACLPath] = v.Path
-
-			if v.Propagate != nil {
-				aclEntry[mkResourceVirtualEnvironmentGroupACLPropagate] = bool(*v.Propagate)
-			} else {
-				aclEntry[mkResourceVirtualEnvironmentGroupACLPropagate] = false
-			}
-
-			aclEntry[mkResourceVirtualEnvironmentGroupACLRoleID] = v.RoleID
-
-			aclParsed = append(aclParsed, aclEntry)
-		}
-	}
-
-	err = d.Set(mkResourceVirtualEnvironmentGroupACL, aclParsed)
-	diags = append(diags, diag.FromErr(err)...)
 
 	if group.Comment != nil {
 		err = d.Set(mkResourceVirtualEnvironmentGroupComment, group.Comment)
 	} else {
 		err = d.Set(mkResourceVirtualEnvironmentGroupComment, "")
 	}
+
 	diags = append(diags, diag.FromErr(err)...)
 
 	err = d.Set(mkResourceVirtualEnvironmentGroupMembers, group.Members)
@@ -202,6 +123,7 @@ func groupRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.
 
 func groupUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	config := m.(proxmoxtf.ProviderConfiguration)
+
 	api, err := config.GetClient()
 	if err != nil {
 		return diag.FromErr(err)
@@ -219,86 +141,18 @@ func groupUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) dia
 		return diag.FromErr(err)
 	}
 
-	aclArgOld, aclArg := d.GetChange(mkResourceVirtualEnvironmentGroupACL)
-	aclParsedOld := aclArgOld.(*schema.Set).List()
-
-	for _, v := range aclParsedOld {
-		aclDelete := types.CustomBool(true)
-		aclEntry := v.(map[string]interface{})
-		aclPropagate := types.CustomBool(
-			aclEntry[mkResourceVirtualEnvironmentGroupACLPropagate].(bool),
-		)
-
-		aclBody := &access.ACLUpdateRequestBody{
-			Delete:    &aclDelete,
-			Groups:    []string{groupID},
-			Path:      aclEntry[mkResourceVirtualEnvironmentGroupACLPath].(string),
-			Propagate: &aclPropagate,
-			Roles:     []string{aclEntry[mkResourceVirtualEnvironmentGroupACLRoleID].(string)},
-		}
-
-		err := api.Access().UpdateACL(ctx, aclBody)
-		if err != nil {
-			return diag.FromErr(err)
-		}
-	}
-
-	aclParsed := aclArg.(*schema.Set).List()
-
-	for _, v := range aclParsed {
-		aclDelete := types.CustomBool(false)
-		aclEntry := v.(map[string]interface{})
-		aclPropagate := types.CustomBool(
-			aclEntry[mkResourceVirtualEnvironmentGroupACLPropagate].(bool),
-		)
-
-		aclBody := &access.ACLUpdateRequestBody{
-			Delete:    &aclDelete,
-			Groups:    []string{groupID},
-			Path:      aclEntry[mkResourceVirtualEnvironmentGroupACLPath].(string),
-			Propagate: &aclPropagate,
-			Roles:     []string{aclEntry[mkResourceVirtualEnvironmentGroupACLRoleID].(string)},
-		}
-
-		err := api.Access().UpdateACL(ctx, aclBody)
-		if err != nil {
-			return diag.FromErr(err)
-		}
-	}
-
 	return groupRead(ctx, d, m)
 }
 
 func groupDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	config := m.(proxmoxtf.ProviderConfiguration)
+
 	api, err := config.GetClient()
 	if err != nil {
 		return diag.FromErr(err)
 	}
 
-	aclParsed := d.Get(mkResourceVirtualEnvironmentGroupACL).(*schema.Set).List()
 	groupID := d.Id()
-
-	for _, v := range aclParsed {
-		aclDelete := types.CustomBool(true)
-		aclEntry := v.(map[string]interface{})
-		aclPropagate := types.CustomBool(
-			aclEntry[mkResourceVirtualEnvironmentGroupACLPropagate].(bool),
-		)
-
-		aclBody := &access.ACLUpdateRequestBody{
-			Delete:    &aclDelete,
-			Groups:    []string{groupID},
-			Path:      aclEntry[mkResourceVirtualEnvironmentGroupACLPath].(string),
-			Propagate: &aclPropagate,
-			Roles:     []string{aclEntry[mkResourceVirtualEnvironmentGroupACLRoleID].(string)},
-		}
-
-		err = api.Access().UpdateACL(ctx, aclBody)
-		if err != nil {
-			return diag.FromErr(err)
-		}
-	}
 
 	err = api.Access().DeleteGroup(ctx, groupID)
 
@@ -308,6 +162,7 @@ func groupDelete(ctx context.Context, d *schema.ResourceData, m interface{}) dia
 
 			return nil
 		}
+
 		return diag.FromErr(err)
 	}
 
